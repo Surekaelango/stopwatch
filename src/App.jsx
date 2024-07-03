78bf8b3118d5e046ddd6ff652d4e9034d70280a5
Surekaelango <sureka0204@gmail.com>
2024-07-03 13:02:59 +0530

    Initial commit done.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './components/StopWatch/StopWatch';
+
+
+const App = () => {
+  return (
+    <div>
+      <StopWatch/>
+      
+    </div>
+  );
+};
+
+export default App;
